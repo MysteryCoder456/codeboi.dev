447430c24b4be2bd4e5b7109c2d7fdcfc4e1d278
Rehatbir Singh <rehatbir.singh@gmail.com>
2024-02-01 14:29:40 +0400

    increased margin

--- src/app/projects.rs
+++ src/app/projects.rs
@@ -191,7 +191,7 @@ pub fn ProjectCard<'a>(project: &'a Project) -> impl IntoView {
             border: 1px solid var(--dim-gray);
             border-radius: 12px;
 
-            margin: 26px 0px;
+            margin: 32px 0px;
             display: grid;
             grid-template-rows: 1fr max-content;
         }
